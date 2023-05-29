c9b744a52e96d5fd887038ec4bf1e994e3577ef2
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-05-29 15:02:42 +0300

    fix some code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { StyledLink } from './Layout.styled';
 
 export const Layout = () => {
