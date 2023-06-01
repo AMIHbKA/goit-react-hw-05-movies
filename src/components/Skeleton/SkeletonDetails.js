0ba2de83bfd4711f3408863360673ba9b57fa878
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-06-01 15:14:56 +0300

    Вернул MovieWrapper

--- src/components/Skeleton/SkeletonDetails.js
+++ src/components/Skeleton/SkeletonDetails.js
@@ -1,6 +1,6 @@
 import {
   MovieDetailsContainer,
-  //MovieWrapper,
+  MovieWrapper,
 } from 'components/MovieCard/MovieCardStyled';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
@@ -8,7 +8,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 export const SkeletonDetails = () => {
   console.log('рендер SkeletonDetails')
   return (
-    //<MovieWrapper>
+    <MovieWrapper>
       <MovieDetailsContainer>
         <div className="poster">
           <Skeleton width={300} height={450} />
@@ -32,6 +32,6 @@ export const SkeletonDetails = () => {
           </p>
         </div>
       </MovieDetailsContainer>
-    //</MovieWrapper>
+    </MovieWrapper>
   );
 };
